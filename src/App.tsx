dd4c1830b3ac89d767ea4a3f3c21bd2a28a57f53
Janine Martins dos Santos <janine.martins@estudante.ufjf.br>
2025-11-01 17:36:23 -0300

    hidratacao do componente pele

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,23 @@ function Pele({tipo, adversidade,status}: PeleProps) {
   )
 
 }
+const listaPeles:PeleProps[]=[
+  {tipo:"aaa",adversidade:"bbb",status:"ccc"},
+   {tipo:"aaa1",adversidade:"bbb1",status:"ccc1"},
+    {tipo:"aa2",adversidade:"bbb2",status:"ccc2"}
+]
 
 function App() {
   
   return (
     <div>
       <h1>AAAAAAAA</h1>
-      <Pele tipo='aaa' adversidade='bbb' status='ccc'></Pele>
-    </div>
+      { 
+        listaPeles.map(
+          (pele)=>
+              <Pele tipo={pele.tipo} adversidade={pele.adversidade} status={pele.status}></Pele>
+        )}
+            </div>
 
     
   )
